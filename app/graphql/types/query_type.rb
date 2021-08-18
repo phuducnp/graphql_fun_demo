088c173095e6753a2169f1ff9a72c03bb554aaea
ducnpp <ducnpp@runsystem.net>
2021-08-18 15:18:34 +0700

    init project

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,23 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    # /users
+    field :users, [Types::UserType], null: false
+
+    def users
+      User.all
+    end
+
+    # /user/:id
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def user(id:)
+      User.find(id)
+    end
+  end
+end
